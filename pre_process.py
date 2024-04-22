77211689e13d4922f0981aa0e2f62dc8b7714a2d
TSOTOF <shm5628@163.com>
2024-04-22 17:40:29 +0800

    完善填充非平衡面板缺失值功能

--- pre_process.py
+++ pre_process.py
@@ -87,13 +87,15 @@ def pretty_stack(df_stack,date_format):
     return df_pretty
 
 
-def stack_balance(df_stack):
+def stack_balance(df_stack,filllimit = 10):
     '''
     描述:
-    填充堆栈的非平衡面板缺失值(对于上市前的数据为空的情况不做填充,最多向前填充10个缺失数据)
+    填充堆栈的非平衡面板缺失值(对于上市前的数据为空的情况不做填充,默认最多向前填充10条缺失数据)
 
     输入变量:
     df_stack:堆栈的DataFrame,第一列为日期(datetime.date),第二列为股票代码(str),shape = [T*N,2 + n]
+
+    filllimit:int,前向填充缺失值的最大数量,默认为10条
 
     输出变量:
     df_balance:填充缺失值之后的DataFrame
@@ -103,7 +105,7 @@ def stack_balance(df_stack):
     df_balance = df_stack.set_index(['date','code'])
     df_balance = df_balance.reindex(newidx).reset_index()
     df_balance.sort_values(['code','date'],inplace = True,ascending = True)
-    df_balance = df_balance.groupby('code',group_keys = False).apply(lambda x: x.fillna(method = 'ffill',limit = 10)).reset_index(drop = True)
+    df_balance = df_balance.groupby('code',group_keys = False).apply(lambda x: x.fillna(method = 'ffill',limit = filllimit)).reset_index(drop = True)
     df_balance = df_balance.dropna(how = 'any')
     return df_balance
 
